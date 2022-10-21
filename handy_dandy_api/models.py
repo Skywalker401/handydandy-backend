560f2bf809377d171e9c6e845c3a04e3c616fda5
AoifeWM <aoifewmulligan@gmail.com>
2022-10-20 20:11:24 -0700

    added basic skeletons of models for database

--- handy_dandy_api/models.py
+++ handy_dandy_api/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -14,3 +15,27 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class HomeArea(models.Model):
+    name = models.CharField(max_length=32)
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    notes = models.TextField(default='')
+    # tasks = []
+    # appliances = []
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=64)
+    appliance = models.CharField(max_length=32)
+    category = models.CharField(max_length=24)
+    description = models.TextField(default='')
+    diy_links = models.TextField(default='')
+    period_months = models.IntegerField()
+    last_performed = models.DateField()
+
+
+class Appliance(models.Model):
+    name = models.CharField(max_length=32)
+    model = models.CharField(max_length=128)
+    serial_number = models.CharField(max_length=128)
