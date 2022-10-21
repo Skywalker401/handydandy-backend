67db9e982122b37d1bcead2ec0b521866429c8d7
S14mx <sergey.otryshko@gmail.com>
2022-10-20 19:13:53 -0700

    implemented auth0

--- handy_dandy_api/urls.py
+++ handy_dandy_api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import UserList, UserDetail
+from . import views
 
 urlpatterns = [
     path('', UserList.as_view(), name='user_list'),
-    path('<int:pk>', UserDetail.as_view(), name='user_detail')
+    path('<int:pk>', UserDetail.as_view(), name='user_detail'),
+    path('api/public', views.public),
+    path('api/private', views.private),
+    path('api/private-scoped', views.private_scoped),
 ]
---
+++ handy_dandy_api/utils.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import authenticate
+import json
+import jwt
+import requests
+import environ
+
+environ.Env.read_env()
+
+
+def jwt_get_username_from_payload_handler(payload):
+    username = payload.get('sub').replace('|', '.')
+    authenticate(remote_user=username)
+    return username
+
+
+def jwt_decode_token(token):
+    header = jwt.get_unverified_header(token)
+    jwks = requests.get(
+        # ENV FORMAT GOES HERE INSTEAD OF THE EMPTY STRING
+        'https://{}/.well-known/jwks.json'.format('')).json()
+    public_key = None
+    for jwk in jwks['keys']:
+        if jwk['kid'] == header['kid']:
+            public_key = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(jwk))
+
+    if public_key is None:
+        raise Exception('Public key not found.')
+
+    # ENV FORMAT GOES HEREINSTEAD OF THE EMPTY STRING
+    issuer = 'https://{}/'.format('')
+
+    return jwt.decode(token, public_key, audience='', issuer=issuer, algorithms=['RS256'])
+    # ENV AUDIENCE GOES HERE INSTEAD OF THE EMPTY STRING
