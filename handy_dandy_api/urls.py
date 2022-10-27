2f6c5d562efdfbed20a79e92627a8c84d485c663
S14mx <sergey.otryshko@gmail.com>
2022-10-27 15:06:39 -0700

    added update route/functionality to tasks

--- handy_dandy_api/urls.py
+++ handy_dandy_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserList, UserDetail, delete_task, public, private, private_scoped, get_user, create_user, create_task, delete_task, get_pros, TaskList, TaskDetail, UserList
+from .views import UserList, UserDetail, delete_task, public, private, private_scoped, get_user, create_user, create_task, delete_task, get_pros, update_task, TaskList, TaskDetail, UserList
 
 urlpatterns = [
     path('', UserList.as_view(), name='user_list'),
@@ -11,6 +11,7 @@ urlpatterns = [
     path('get-pros', get_pros),
     path('create-user', create_user),
     path('create-task', create_task),
+    path('update-task', update_task),
     path('delete-task', delete_task),
     path('task-list', TaskList.as_view(), name='task_list'),
     path('task/<int:pk>', TaskDetail.as_view(), name='task_detail'),
